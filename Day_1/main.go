119922f5e31cc555216d792c4fe5e097782914ad
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-12-05 13:50:52 +0100

    Day 1

---
+++ Day_1/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	result := puzzle2()
+	println(result)
+}
+
+func puzzle1() int {
+	bigfood := 0
+	curentfood := 0
+
+	file, err := os.Open("in.txt")
+	defer file.Close()
+	if err != nil {
+		println(err)
+	} else {
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			a, _ := strconv.Atoi(scanner.Text())
+			if a == 0 {
+				curentfood = 0
+			} else {
+				curentfood += a
+				if curentfood > bigfood {
+					bigfood = curentfood
+				}
+			}
+		}
+	}
+	return (bigfood)
+}
+
+func puzzle2() int {
+	var arrFood [1000]int
+	i := 0
+	file, err := os.Open("in.txt")
+	defer file.Close()
+	if err != nil {
+		println(err)
+	} else {
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			a, _ := strconv.Atoi(scanner.Text())
+			if a != 0 {
+				arrFood[i] += a
+			} else {
+				i++
+			}
+		}
+	}
+
+	arrSlice := arrFood[:]
+	sort.Sort(sort.Reverse(sort.IntSlice(arrSlice)))
+	res := arrFood[0] + arrFood[1] + arrFood[2]
+
+	return (res)
+}
