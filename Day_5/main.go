e61cd8c8bd5d81efd9c9d1ae7f4a24e5f1383c3f
maxbernard <max.bernard@algosup.com>
2022-12-06 22:43:40 +0100

    Day 5

--- Day_5/main.go
+++ Day_5/main.go
@@ -8,21 +8,14 @@ import (
 )
 
 func main() {
-	/*a := readInstruction("move 2 from 2 to 1")
-	b := crateToArr()
-	res := moveCrate(b, a)
-	for i := 0; i < 9; i++ {
-		println(b[i][0])
-		println("-------")
-		println(b[i][1])
-	}*/
-	a := readInstruction("move 2 from 2 to 1")
-	b := crateToArr()
-	moveCrate(b, a)
+	result1 := puzzle1()
+	println(result1)
+	result2 := puzzle2()
+	println(result2)
 }
 
-func puzzle1() int {
-	res := 0
+func puzzle1() string {
+	crate := crateToArr()
 	file, err := os.Open("in.txt")
 	defer file.Close()
 	if err != nil {
@@ -30,15 +23,18 @@ func puzzle1() int {
 	} else {
 		scanner := bufio.NewScanner(file)
 		for scanner.Scan() {
-
+			inst := readInstruction(scanner.Text())
+			crate = moveCrate(crate, inst)
 		}
 	}
+
+	res := topCrate(crate)
 
 	return (res)
 }
 
-func puzzle2() int {
-	res := 0
+func puzzle2() string {
+	crate := crateToArr()
 	file, err := os.Open("in.txt")
 	defer file.Close()
 	if err != nil {
@@ -46,16 +42,18 @@ func puzzle2() int {
 	} else {
 		scanner := bufio.NewScanner(file)
 		for scanner.Scan() {
-
+			inst := readInstruction(scanner.Text())
+			crate = moveCrateStacks(crate, inst)
 		}
 	}
+
+	res := topCrate(crate)
 
 	return (res)
 }
 
-func crateToArr() [100][9]string {
-	var strRes [100][9]string
-	j := 0
+func crateToArr() [9][]string {
+	var strRes [9][]string
 	file, err := os.Open("crate.txt")
 	defer file.Close()
 	if err != nil {
@@ -66,11 +64,15 @@ func crateToArr() [100][9]string {
 			scanner.Text()
 			k := 0
 			for i := 1; k < 9; i += 4 {
-				strRes[j][k] = string(scanner.Text()[i])
+				if string(scanner.Text()[i]) != " " {
+					strRes[k] = append(strRes[k], string(scanner.Text()[i]))
+				}
 				k++
 			}
-			j++
 		}
+	}
+	for i := 0; i < 9; i++ {
+		strRes[i] = reverse(strRes[i])
 	}
 
 	return strRes
@@ -84,22 +86,49 @@ func readInstruction(in string) [3]int {
 	for i := 0; i < len(a); i++ {
 		res[i], _ = strconv.Atoi(a[i])
 	}
+	res[1] = res[1] - 1
+	res[2] = res[2] - 1
 
 	return res
 }
 
-func moveCrate(crate [100][9]string, in [3]int) [100][9]string {
-	tempCrate := crate
-	maxout := 0
-	maxin := 0
-	for i := 0; i < 99; i++ {
-		if crate[i][in[1]] == "" {
-			maxout = i
-		}
-		if crate[i][in[2]] == "" {
-			maxin = i
-		}
+func moveCrate(crate [9][]string, in [3]int) [9][]string {
+
+	for i := 0; i < in[0]; i++ {
+		lenRm := len(crate[in[1]]) - 1
+		crate[in[2]] = append(crate[in[2]], crate[in[1]][lenRm])
+		crate[in[1]] = crate[in[1]][:lenRm]
 	}
-	println(maxout, maxin)
-	return tempCrate
+
+	return crate
+}
+
+func moveCrateStacks(crate [9][]string, in [3]int) [9][]string {
+
+	lenRm := len(crate[in[1]]) - in[0]
+
+	tempCrate := crate[in[1]][lenRm:]
+
+	for i := 0; i < len(tempCrate); i++ {
+		crate[in[2]] = append(crate[in[2]], tempCrate[i])
+	}
+	crate[in[1]] = crate[in[1]][:lenRm]
+
+	return crate
+}
+
+func topCrate(crate [9][]string) string {
+	res := ""
+	for i := 0; i < 9; i++ {
+		res += crate[i][len(crate[i])-1]
+	}
+	return res
+}
+
+func reverse(input []string) []string {
+	var output []string
+	for i := len(input) - 1; i >= 0; i-- {
+		output = append(output, input[i])
+	}
+	return output
 }
